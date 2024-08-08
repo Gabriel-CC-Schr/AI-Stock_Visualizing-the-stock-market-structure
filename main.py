5555c0aad2e6f9e9a943d67413be181ad3ead670
Gabriel <ccgabriel.888@gmail.com>
2024-08-07 18:10:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pandas_datareader as web
 import datetime as dt
 
 from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, LSTM
+from tensorflow.keras.models import Sequential  # type: ignore
+from tensorflow.keras.layers import Dense, Dropout, LSTM  # type: ignore
 
 # Load Data
 company = 'FB'
@@ -58,3 +58,14 @@ actual_prices = test_data['Close'].values
 total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
 
 model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model_inputs.reshape(-1, 1)
+model_inputs = scaler.transform(model_inputs)
+
+# Make Predictions on Test Data
+
+x_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x-prediction_days:x, 0])
+
+x_test = np.array(x_test)
