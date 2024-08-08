0611ad6eafd9fe3978b7ae9532fd8ef14984fc09
Gabriel <ccgabriel.888@gmail.com>
2024-08-07 17:53:03 -0700

    Update main.py

--- main.py
+++ main.py
@@ -36,3 +36,25 @@ x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 model = Sequential()
 
 model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1)) # Prediction of the next closing value
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+''' Test The Model Accuracy on Existing Data '''
+
+# Load Test Data
+test_start = dt.datetime(2020, 1, 1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(company, 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
